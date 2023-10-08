30fbb91904bc37f5c78f6cfd02ed10df9c74fe11
Nabadeep <nabadeep25@gmail.com>
2023-10-08 14:18:27 +0530

    feat: :sparkles: cors settings added

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const appRouter = require("./src/routes/index");
 const app = express();
-
+var cors = require('cors')
 app.use(express.json());
 
+app.use(cors())
 app.use('/api/v1',appRouter);
 
 module.exports=app;
